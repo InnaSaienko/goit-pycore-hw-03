641a9b8d268be93a80fb6158c3ac987a34ffdb62
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-08 22:06:48 +0200

    task 1: refactor exception handling

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 
-def get_days_from_today(date):
+
+def get_days_from_today(date: str):
     try:
         date_obj = datetime.strptime(date, "%Y-%m-%d").date()
-        today = datetime.today().date()
-        #today = datetime.strptime("2021-05-05", "%Y-%m-%d").date()
-        return (today - date_obj).days
     except ValueError:
         return "Invalid date format. Use YYYY-MM-DD"
 
+    # today = datetime.strptime("2021-05-05", "%Y-%m-%d").date()
+    today = datetime.today().date()
+    return (today - date_obj).days
 
-get_days_from_today("2021-10-09")
-print(get_days_from_today("2021-10-09"))
+
+days_diff = get_days_from_today("2021-10-09")
+print(days_diff)
