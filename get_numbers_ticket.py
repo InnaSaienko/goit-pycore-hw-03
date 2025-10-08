e3c5af7cb17c50a0b843ac28a3899bf7e5da8d00
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-08 22:08:41 +0200

    task 2: change quantity comparison expression

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -1,10 +1,10 @@
 import random
 
 def get_numbers_ticket(min_num, max_num, quantity):
-    if min_num < 1 or max_num > 1000 or quantity < 1 or quantity > (max_num - min_num + 1):
+    if min_num < 1 or max_num > 1000 or 1 > quantity > (max_num - min_num + 1):
         return []
 
     return sorted(random.sample(range(min_num, max_num + 1), quantity))
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+print("Your lottery numbers:", lottery_numbers)
